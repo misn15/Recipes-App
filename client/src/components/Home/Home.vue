33e01a5eb50cdcb93d7ae1e29a2ec3397b133e22
Nicole Mis <misn15@bu.edu>
2022-02-17 15:29:09 -0500

    cleaning up Home page

--- client/src/components/Home/Home.vue
+++ client/src/components/Home/Home.vue
@@ -1,60 +1,41 @@
 <template>
     <div class="bg">
         <v-container mb-12 class="mainTxt">
-        <h1>Welcome</h1>
-        <p class="greetingText">Find new recipes to spice up your week nights and make eating exciting</p>
+            <h1>Welcome</h1>
+            <p class="greetingText">Find new recipes to spice up your week nights and make eating exciting</p>
         </v-container>
-            <v-row fill-height class="d-flex justify-center align-center">
-                <v-btn elevation="2" width="11%" outlined color="black" @click="$router.push('search')"> 
-                     Search Recipes
-                </v-btn>
-            </v-row>
-                <v-row class="ma-10 d-flex justify-center">
-                    Or
-                </v-row>
-            <v-row class="d-flex justify-center">
-                <!-- <v-col cols="12"> -->
-                <v-btn elevation="2" width="11%" outlined color="black" @click="$router.push('signup')"> 
-                     Sign Up
-                </v-btn>
-                <!-- <v-spacer width="1%"></v-spacer> -->
-                <v-btn elevation="2" outlined width="11%" color="black" @click="$router.push('signin')">
-                    Log In
-                </v-btn>   
-                <!-- <v-spacer width="1%"></v-spacer> -->
-                <!-- <v-btn elevation="2" outlined width="11%" color="black">
+        <v-row fill-height class="d-flex justify-center align-center">
+            <v-btn elevation="2" outlined color="black" @click="$router.push('search')"> 
                     Search Recipes
-                </v-btn>    -->
-                <!-- </v-col> -->
-            </v-row>
-       
-    </div>
-    
+            </v-btn>
+        </v-row>
+        <v-row class="ma-10 d-flex justify-center">
+            Or
+        </v-row>
+        <v-row class="d-flex justify-center">
+            <v-btn elevation="2" width="11%" outlined color="black" @click="$router.push('signup')"> 
+                    Sign Up
+            </v-btn>
+            <v-btn elevation="2" outlined width="11%" color="black" @click="$router.push('signin')">
+                Log In
+            </v-btn>   
+        </v-row>
+    </div>  
 </template>
 
 <script>
 
 export default {
     props: {
-        msg: {
-            String
-            }
-    },
+        },
     data() {
-        return { 
-            isFavorited: false
-        }
-    },
+        },
     components: {
-    },
+        },
     methods: {
-        toggle(){
-            this.isFavorited ? this.isFavorited=false : this.isFavorited=true;
-            console.log(this.isFavorited);
-        }
-    },
+        },
     computed: {
-    },
+        },
 }
     
 </script>
@@ -66,10 +47,6 @@ export default {
     height: 100%;
 }
 
-.ct {
-padding-bottom: 30%;
-}
-
 .greetingText {
     font-family: Verdana;
     font-size: 1.2em;
@@ -77,11 +54,6 @@ padding-bottom: 30%;
 
 .mainTxt {
     padding-top: 5%;
-}
-
-.txt {
-    text-align: center;
-
 }
 
 </style>
